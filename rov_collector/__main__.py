dbccbfea91befd49fb1820120bf1458f16270cac
Justin Furuness <jfuruness@gmail.com>
2023-11-28 03:18:21 -0500

    mypy

--- rov_collector/__main__.py
+++ rov_collector/__main__.py
@@ -6,7 +6,8 @@ from .collectors import rov_collector_classes
 def main():
     json_path: Path = Path.home() / "Desktop" / "rov_info.json"
     for CollectorCls in rov_collector_classes:
-        CollectorCls(json_path=json_path).run()
+        # For some reason mypy is freaking out about the instantiation here
+        CollectorCls(json_path=json_path).run()  # type: ignore
 
 
 if __name__ == "__main__":
