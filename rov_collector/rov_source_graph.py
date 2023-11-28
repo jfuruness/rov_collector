915d93650b753e3ce284f29727e41039bb5b1525
Justin Furuness <jfuruness@gmail.com>
2023-11-28 04:33:23 -0500

    added four roa collectors, along with tests and a graph

--- rov_collector/rov_source_graph.py
+++ rov_collector/rov_source_graph.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from typing import Optional
 
 import matplotlib.pyplot as plt
 
@@ -10,21 +11,40 @@ class ROVSourceGraph:
     def __init__(self, json_path: Path) -> None:
         self.json_path: Path = json_path
 
-    def run(self) -> None:
+    def run(self, out_dir: Optional[Path] = None) -> None:
         """Counts number of entries for each ASN and plots them"""
 
         source_counts = self._get_counts()
-        sources = list(source_counts.keys())
-        counts = list(source_counts.values())
+
+        # Sorting the sources based on counts in descending order
+        sorted_sources_counts = sorted(
+            source_counts.items(), key=lambda item: item[1], reverse=True
+        )
+        sources, counts = zip(*sorted_sources_counts)
 
         # Creating the bar graph
         plt.figure(figsize=(10, 6))
-        plt.bar(sources, counts, color="skyblue")
+        bars = plt.bar(sources, counts, color="skyblue")
         plt.xlabel("Source")
         plt.ylabel("Number of ASes")
         plt.title("Number of ASes per Source")
         plt.xticks(rotation=45)
-        plt.show()
+
+        # Adding the count above each bar
+        for bar in bars:
+            yval = bar.get_height()
+            plt.text(
+                bar.get_x() + bar.get_width() / 2,
+                yval,
+                int(yval),
+                va="bottom",
+                ha="center",
+            )
+
+        dir_ = out_dir if out_dir else self.json_path.parent / "rov_source_counts.png"
+        plt.savefig(dir_, bbox_inches="tight")
+        plt.close()
+        print(f"Saved to {dir_}")
 
     def _get_counts(self) -> dict[str, int]:
         counts = {x.value: 0 for x in list(Source)}
